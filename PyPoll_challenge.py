10f8f8a3e4e9a46c19980abb9c2cb440a6fa48ee
Sukurudo <35831741+Sukurudo@users.noreply.github.com>
2020-02-02 01:19:18 -0800

    Upload Challenge Complete data

--- PyPoll_challenge.py
+++ PyPoll_challenge.py
@@ -18,14 +18,24 @@ total_votes = 0
 #Candidate Options
 candidate_options = []
 
+#County Options
+county_options = []
+
 #candidate Votes Dictionary
 candidate_votes = {}
+
+#County Votes Dictionary
+county_votes = {}
 
 #initalize winner data
 #winner winner chicken dinner
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
+
+winning_county = ""
+winning_county_count = 0
+winning_county_percentage = 0
 
 
 #Open CSV to begin processing
@@ -40,6 +50,21 @@ with open(electioncsv, newline='') as election_data:
     for row in file_reader:
         # 1. Total number of Votes Cast
         total_votes += 1
+
+#Complete List of Counties and number of Votes
+        #Add countie names from each row
+        county_name = row[1]
+
+        #add unique Counties to county options
+        if county_name not in county_options:
+            #Add county names to List
+            county_options.append(county_name)
+            #Track County Votes
+            county_votes[county_name] = 0
+
+        #increment County Votes
+        county_votes[county_name] += 1
+
 
 # 2. A Complete List of candidates who recieved Votes
 
@@ -64,11 +89,44 @@ with open(electionresults, "w") as txt_file:
     f"\nElection Results\n"
     f"-------------------------\n"
     f"Total Votes: {total_votes:,}\n"
-    f"-------------------------\n")
+    f"-------------------------\n"
+    f"\n"
+    f"County Votes:\n")
     print(election_results, end="")
     # Save the final vote count to the text file.
     txt_file.write(election_results)
 
+    # County Breakdown of Votes
+    for county in county_votes:
+        #Retrieve Vote Count
+        Cvotes = county_votes[county]
+        #Calculate County Vote Percentage
+        Cvote_percentage = int(Cvotes) / int(total_votes) * 100
+
+        #Find Highest County Turnout
+        if (Cvotes > winning_county_count) and (Cvote_percentage > winning_county_percentage):
+            #Set current County as top Performer
+            winning_county_count = Cvotes
+            winning_county_percentage = Cvote_percentage
+            winning_county = county
+
+        #Print County Results
+        county_results = (f"{county}: {Cvote_percentage:.1f}% ({Cvotes:,})\n")
+        print(county_results)
+        txt_file.write(county_results)
+
+    #County Highest Turnout
+    winning_county_summary = (
+        f"-------------------------\n"
+        f"Largest County Turnout: {winning_county} "
+        f"with {winning_county_percentage:.1f}% of all Votes\n"
+        f"-------------------------\n\n")
+    print(winning_county_summary)
+    txt_file.write(winning_county_summary) 
+
+    candidate_header = ("Candidate Vote Analysis:\n")
+    print(candidate_header)
+    txt_file.write(candidate_header)
 
     # 3. Percentage breakdown of votes for each Candidate
     for candidate in candidate_votes:
@@ -86,8 +144,6 @@ with open(electionresults, "w") as txt_file:
             winning_candidate = candidate
 
         #Print Vote Percentages
-        # print(f"{candidate}: received {vote_percentage:.1f} % of the vote")
-        # print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
         candidate_results = (f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
         print(candidate_results)
         txt_file.write(candidate_results)
@@ -102,16 +158,4 @@ with open(electionresults, "w") as txt_file:
         f"-------------------------\n")
     print(winning_candidate_summary)
     txt_file.write(winning_candidate_summary)
-
-
-
-
-
-# Print Total Votes (Debug)
-# print(total_votes)
-# print(candidate_options)
-# print(candidate_votes)
-
-
-
 
