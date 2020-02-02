f20e3ec34d46f5e82f039a9af0c0db74979d9cca
Sukurudo <35831741+Sukurudo@users.noreply.github.com>
2020-02-01 23:54:39 -0800

    Adding election audit and winner sections.

--- PyPol.py
+++ PyPol.py
@@ -12,6 +12,21 @@ electioncsv = os.path.join('Resources', 'election_results.csv')
 #Path to Output File
 electionresults = os.path.join('analysis', 'election_analysis.txt')
 
+#Initalize Vote Tally
+total_votes = 0
+
+#Candidate Options
+candidate_options = []
+
+#candidate Votes Dictionary
+candidate_votes = {}
+
+#initalize winner data
+#winner winner chicken dinner
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 
 #Open CSV to begin processing
 with open(electioncsv, newline='') as election_data:
@@ -20,13 +35,68 @@ with open(electioncsv, newline='') as election_data:
 
     #Read and Print Header Row
     headers = next(file_reader)
-    print(headers)
 
+    #Analyze the rows
+    for row in file_reader:
+        # 1. Total number of Votes Cast
+        total_votes += 1
 
-
-
-# 1. Total number of Votes Cast
 # 2. A Complete List of candidates who recieved Votes
+
+        # Add Candidate Names from each Row
+        candidate_name = row[2]
+
+        #Add unique Candidates to Candidate Options
+        if candidate_name not in candidate_options:
+            #add Candidate names to List
+            candidate_options.append(candidate_name)
+            #Track individual Candidate Votes
+            candidate_votes[candidate_name] = 0
+
+        #Increment Candidate Vote Count
+        candidate_votes[candidate_name] += 1
+
+#Headers
+print("\n\nElection Results\n________________\n")
+
 # 3. Percentage breakdown of votes for each Candidate
-# 4. Total number of votes per candidate
+for candidate in candidate_votes:
+    #Retrieve vote count
+    votes = candidate_votes[candidate]
+    #Calculate Percentages
+    vote_percentage = int(votes) / int(total_votes) * 100
+
+    #figure out the Winner
+    #Is current candidate the Winner?
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        #Set current candidate as winner and store data
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate
+
+    #Print Vote Percentages
+    # print(f"{candidate}: received {vote_percentage:.1f} % of the vote")
+    
+    print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+
 # 5. Winner of election based on Popular Vote (Most Votes)
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
+
+
+
+
+# Print Total Votes (Debug)
+# print(total_votes)
+# print(candidate_options)
+# print(candidate_votes)
+
+
+
+
